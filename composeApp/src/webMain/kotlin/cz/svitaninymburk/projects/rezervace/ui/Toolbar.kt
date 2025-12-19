efc18a55ff75aa1a7b5af3f5537fb5775469b551
lukas.brhlik <lukas.brhlik@juicymo.cz>
2025-12-19 14:51:23 +0100

    Add logo, prepare toolbar

--- composeApp/src/webMain/kotlin/cz/svitaninymburk/projects/rezervace/ui/Toolbar.kt
+++ composeApp/src/webMain/kotlin/cz/svitaninymburk/projects/rezervace/ui/Toolbar.kt
@@ -1,16 +1,19 @@
 package cz.svitaninymburk.projects.rezervace.ui
 
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MediumTopAppBar
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import org.jetbrains.compose.resources.vectorResource
+import rezervace.composeapp.generated.resources.Res
+import rezervace.composeapp.generated.resources.Svitani_small
 
 @Composable fun Toolbar(
     title: String,
 ) {
     MediumTopAppBar(
         title = { Text(title) },
-        navigationIcon = { TODO("AppLogo") },
+        navigationIcon = { Icon(vectorResource(Res.drawable.Svitani_small), contentDescription = null) },
         actions = { TODO("hiding navigation + user") },
-        colors = TODO("AppColors"),
     )
 }
