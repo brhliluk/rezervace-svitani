839e027cd70dde9ed995d8f6515d599144269d3f
lukas.brhlik <lukas.brhlik@juicymo.cz>
2025-12-19 11:15:57 +0100

    Upgrade dependencies and add adaptive navigation libraries

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -19,12 +19,14 @@ dependencyResolutionManagement {
     repositories {
         google {
             mavenContent {
+                includeGroupAndSubgroups("android")
                 includeGroupAndSubgroups("androidx")
                 includeGroupAndSubgroups("com.android")
                 includeGroupAndSubgroups("com.google")
             }
         }
         mavenCentral()
+        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
     }
 }
 
